4c9523b6dcc534d0715a9ebff6a00fc3055684ae
Moaz <moaz2009amen@gmail.com>
2026-05-20 23:10:44 +0300

    Configure Next.js with strict mode and image domains

--- next.config.js
+++ next.config.js
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['rcxynaugevczwovvlyjb.supabase.co'],
+  },
+}
 
+module.exports = nextConfig
